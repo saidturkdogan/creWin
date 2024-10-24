0f790aac6110d4ee5459ca29d543ee3c9e895a76
Said Cemal Türkdoğan <69085609+saidturkdogan@users.noreply.github.com>
2024-10-25 02:58:04 +0300

    project initiation and a few code edits

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,12 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult About()
+    {
+        ViewData["Message"] = "said.";
+        return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
